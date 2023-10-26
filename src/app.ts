5e5c204c600e5e2bd6627647ca58ec949d4fffb9
rohitkhan4141 <rohitkhan4141@gmail,com>
2023-10-26 11:51:34 +0600

    back-end-starter-pack

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import routes from './app/routes';
+
+// import cookieParser from 'cookie-parser';
+
+const app: Application = express();
+
+app.use(cors());
+// app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1', routes);
+
+//Testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing Error logger')
+// })
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle not found
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.status(httpStatus.NOT_FOUND).json({
+//     success: false,
+//     message: 'Not Found',
+//     errorMessages: [
+//       {
+//         path: req.originalUrl,
+//         message: 'API Not Found',
+//       },
+//     ],
+//   });
+//   next();
+// });
+
+export default app;
